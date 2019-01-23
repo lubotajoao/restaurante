74da0480686972b927450b442562aee2b6d032d2
Lubota Joao <lubota4@gmail.com>
2019-01-23 20:04:28 -0200

    Resto Bar [Configuracao do PHPMailer]

--- index.php
+++ index.php
@@ -139,21 +139,82 @@
                $telefone      = $_POST['telefone'];
                $data          = $_POST['data'];
                $num_pessoas   = $_POST['num_pessoas'];
+
+               $nome          = clean_input($nome);
+               $email         = clean_input($email);
+               $mensagem      = clean_input($mensagem);
+               $telefone      = clean_input($telefone);
+               $data          = clean_input($data);
+               $num_pessoas   = clean_input($num_pessoas);
             }
 
-            $nome          = clean_input($nome);
-            $email         = clean_input($email);
-            $mensagem      = clean_input($mensagem);
-            $telefone      = clean_input($telefone);
-            $data          = clean_input($data);
-            $num_pessoas   = clean_input($num_pessoas);
+            // Inserir Arquivos do PHPMailer
+            require 'phpmailer/Exception.php';
+            require 'phpmailer/PHPMailer.php';
+            require 'phpmailer/SMTP.php';
 
-            echo $nome . '<br />';
-            echo $email . '<br />';
-            echo $mensagem . '<br />';
-            echo $telefone . '<br />';
-            echo $data . '<br />';
-            echo $num_pessoas . '<br />';
+            // Usar as classes sem o namespace
+            use PHPMailer\PHPMailer\PHPMailer;
+            use PHPMailer\PHPMailer\Exception;
+
+            // Criação do Objeto da Classe PHPMailer
+            $mail = new PHPMailer(true);
+            try {
+
+               //Retire o comentário abaixo para soltar detalhes do envio
+               // $mail->SMTPDebug = 2;
+
+               // Usar SMTP para o envio
+               $mail->isSMTP();
+
+               // Detalhes do servidor (No nosso exemplo é o Google)
+               $mail->Host = 'smtp.gmail.com';
+
+               // Permitir autenticação SMTP
+               $mail->SMTPAuth = true;
+
+               // Nome do usuário
+               $mail->Username = 'cwebjoao@gmail.com';
+               // Senha do E-mail
+               $mail->Password = 'CursosWeb@1234';
+               // Tipo de protocolo de segurança
+               $mail->SMTPSecure = 'tls';
+
+               // Porta de conexão com o servidor
+               $mail->Port = 587;
+
+
+               // Garantir a autenticação com o Google
+               $mail->SMTPOptions = array(
+                  'ssl' => array(
+                     'verify_peer' => false,
+                     'verify_peer_name' => false,
+                     'allow_self_signed' => true
+                  )
+               );
+
+               // Remetente
+               $mail->setFrom('from@example.com', 'Mailer');
+
+               // Destinatários
+               $mail->addAddress('cwebjoao@gmail.com', 'Cursos Web Joao');
+
+               // Conteúdo
+
+               // Define conteúdo como HTML
+               $mail->isHTML(true);
+
+               // Assunto
+               $mail->Subject = 'Insira o assunto';
+               $mail->Body    = 'Insira o texto do e-mail';
+               $mail->AltBody = 'Formato alternativo em texto puro para emails que não aceitam HTML';
+
+               // Enviar E-mail
+               $mail->send();
+               echo 'Mensagem enviada com sucesso';
+            } catch (Exception $e) {
+               echo 'A mensagem não foi enviada pelo seguinte motivo: ', $mail->ErrorInfo;
+            }
 
             ?>
 
